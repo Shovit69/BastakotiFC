255b4e0370da9d2e9dab6882070aa4bfd61fc872
Shovit69 <78230283+Shovit69@users.noreply.github.com>
2022-04-21 22:49:43 -0400

    Delete Models directory

--- Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BastakotiFC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
