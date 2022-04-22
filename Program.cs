60e0fae13c59bc604464f61b27231fd6a317cf7d
Shovit69 <78230283+Shovit69@users.noreply.github.com>
2022-04-21 22:44:36 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,18 +1,29 @@
-using Microsoft.AspNetCore.Identity;
+using ShovitBastakoti.Models;
 using Microsoft.EntityFrameworkCore;
-using BastakotiFC.Areas.Identity.Data;
+using ShovitBastakoti.Data;
+using Microsoft.AspNetCore.Identity;
+
 var builder = WebApplication.CreateBuilder(args);
-var connectionString = builder.Configuration.GetConnectionString("BastakotiFCContextConnection");;
-
-builder.Services.AddDbContext<BastakotiFCContext>(options =>
-    options.UseSqlServer(connectionString));;
-
-builder.Services.AddDefaultIdentity<BastakotiFCUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddEntityFrameworkStores<BastakotiFCContext>();;
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<BastakotiFCContext>(options =>
+   options.UseSqlServer("Server=tcp:shovit.database.windows.net,1433;Initial Catalog=BastakotiFC;Persist Security Info=False;User ID=admin69;Password=Messidona69@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+builder.Services.AddDbContext<DataContext>(options =>
+                options.UseSqlServer(
+                   "Server=tcp:shovit.database.windows.net,1433;Initial Catalog=BastakotiFC;Persist Security Info=False;User ID=admin69;Password=Messidona69@;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"));
+builder.Services.AddDefaultIdentity<IdentityUser>(
+               options =>
+               {
+                   options.SignIn.RequireConfirmedAccount = true;
+                   options.Password.RequiredLength = 6;
+                   options.Password.RequireLowercase = true;
+                   options.Password.RequireUppercase = true;
+               }
+               )
+               .AddRoles<IdentityRole>()
+               .AddEntityFrameworkStores<BastakotiFCContext>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -27,13 +38,11 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-app.UseAuthentication();;
-
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.MapRazorPages();
-
 app.Run();
